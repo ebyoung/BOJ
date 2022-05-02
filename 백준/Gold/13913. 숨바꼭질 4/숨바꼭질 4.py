78dc96f419086bf72444c0941602a6b1f1d55bed
ebyoung <78834820+ebyoung@users.noreply.github.com>
2022-05-02 18:26:48 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 180 ms, Memory: 125400 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -1,26 +1,11 @@
 from collections import deque
-import sys
-
-
-sys.setrecursionlimit(int(1e6))
-
-
-def find_path(v, cnt, path):
-    if cnt == 1:
-        print(*path[::-1])
-        return 1
-
-    for w in [v + 1, v - 1, v // 2]:
-        if 0 <= w < 100002 and visitied[w] == cnt - 1:
-            path.append(w)
-            if find_path(w, cnt-1, path):
-                return 1
-            path.pop()
 
 
 N, K = map(int, input().split())
 visitied = [0] * 100002
 visitied[N] = 1
+path = [0] * 100002
+path[N] = N
 
 # 가장 빠른 시간 계산
 queue = deque([N])
@@ -32,6 +17,13 @@ while queue:
         if 0 <= w < 100002 and not visitied[w]:
             queue.append(w)
             visitied[w] = visitied[v] + 1
+            path[w] = v
 print(visitied[K] - 1)
 # 가장 빠른 경로 확인
-find_path(K, visitied[K], [K])
+shortest_path = [K]
+idx = K
+while path[idx] != idx:
+    idx = path[idx]
+    shortest_path.append(idx)
+shortest_path.reverse()
+print(*shortest_path)
