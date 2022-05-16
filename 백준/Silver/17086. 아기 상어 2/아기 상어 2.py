1b3c1a45592ddf3848e93a526e18ca16a0ce3c9e
ebyoung <78834820+ebyoung@users.noreply.github.com>
2022-05-16 21:41:23 +0900

    [Silver II] Title: 아기 상어 2, Time: 160 ms, Memory: 116320 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.py
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.py
@@ -1,31 +1,28 @@
 from collections import deque
 
 
-def bfs(s):
-    visited = [[0] * M for _ in range(N)]
-    visited[s[0]][s[1]] = 1
-    queue = deque([s])
-    while queue:
-        v = queue.popleft()
-        if array[v[0]][v[1]]:
-            return visited[v[0]][v[1]]
-
-        for d in dxy:
-            ny = v[0] + d[0]
-            nx = v[1] + d[1]
-            if 0 <= ny < N and 0 <= nx < M and not visited[ny][nx]:
-                visited[ny][nx] = visited[v[0]][v[1]] + 1
-                queue.append((ny, nx))
-
-
 N, M = map(int, input().split())
 array = [list(map(int, input().split())) for _ in range(N)]
 dxy = [(dy, dx) for dy in [-1, 0, 1] for dx in [-1, 0, 1] if dy or dx]
-max_dist = 0
+
+queue = deque([])
 for i in range(N):
     for j in range(M):
-        cur_dist = bfs((i, j))
-        if cur_dist > max_dist:
-            max_dist = cur_dist
+        if array[i][j]:
+            queue.append((i, j))
+
+max_dist = 0
+while queue:
+    v = queue.popleft()
+    cur_dist = array[v[0]][v[1]]
+    if cur_dist > max_dist:
+        max_dist = cur_dist
+
+    for d in dxy:
+        ny = v[0] + d[0]
+        nx = v[1] + d[1]
+        if 0 <= ny < N and 0 <= nx < M and not array[ny][nx]:
+            array[ny][nx] = cur_dist + 1
+            queue.append((ny, nx))
 
 print(max_dist-1)
